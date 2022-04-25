72a50be6d15da70e72ef7bbfb7eb71bcce2ae7e8
tani.hajime <h-tani@i-enter.co.jp>
2022-04-25 06:36:12 +0000

    チャプター９が終わったところ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-        </div>
-    </div>
-    
     @if (Auth::check())
-        {{ Auth::user()->name }}
+        <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
+                {{-- 投稿一覧 --}}
+                @include('microposts.microposts')
+            </div>
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
@@ -18,5 +30,4 @@
             </div>
         </div>
     @endif
-    
 @endsection
